1a7d5a7638a1b3957b50be76e83191a386c3aab1
Dmitriy Chekmarev <dzmitryc@gmail.com>
2016-01-06 18:42:08 +0700

    it was just a notepad file for quick evaluations

--- src/main/java/com/crossover/properties/Main.java
+++ src/main/java/com/crossover/properties/Main.java
@@ -12,31 +12,6 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-  int solution(int n) {
-    int[] d = new int[30];
-    int l = 0;
-    int p;
-    while (n > 0) {
-      d[l] = n % 2;
-      n /= 2;
-      l++;
-    }
-    for (p = 1; p < 1 + l; ++p) {
-      int i;
-      boolean ok = true;
-      for (i = 0; i < l - p; ++i) {
-        if (d[i] != d[i + p]) {
-          ok = false;
-          break;
-        }
-      }
-      if (ok) {
-        return p;
-      }
-    }
-    return -1;
-  }
-
   private static final List<PropertyConfig> PROPERTIES = ImmutableList.of(
       new TextProperty("JDBC_DRIVER", true), // com.mysql.jdbc.Driver
       new TextProperty("JDBC_URL", true), // jdbc:mysql://localhost/test
